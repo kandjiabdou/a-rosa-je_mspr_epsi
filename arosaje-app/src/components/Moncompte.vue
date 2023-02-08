d5bc4ae12bf746788021df028c341483572db9e1
kandjiabdou <abdu.kandji@gmail.com>
2023-02-08 10:34:52 +0100

    akbou kandji

---
+++ arosaje-app/src/components/Moncompte.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    rdfghjklm:
+  </div>
+</template>
+
+<script>
+import myContentDrawer from "@/components/Drawer"
+export default {
+  name: 'moncompte',
+  components: {myContentDrawer},
+  data: function () {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
